d4d253c13987934d7d85ee10f5d503a73bc18f1a
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-06-10 12:36:08 +0530

    setup express object to run server

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const setupAndStartServer =async ()=>{
+    // create an express object
+    const app = express();
+    const PORT =3000;
+    app.listen(PORT , ()=>{
+        console.log(`Server started at ${PORT}`);
+    });
+}
+
+setupAndStartServer();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
