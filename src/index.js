a26ccabe54e9d28b7304a5d3223b40d96dda7a05
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-06-10 13:02:11 +0530

    .env setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const { PORT } = require('./config/serverConfig');
 const bodyParser = require('body-parser');
 
 const setupAndStartServer =async ()=>{
     // create an express object
     const app = express();
-    const PORT =3000;
     app.listen(PORT , ()=>{
         console.log(`Server started at ${PORT}`);
     });
