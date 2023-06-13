720473cf48f01e2d4606bd183d21670148cb5d42
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-06-13 12:55:48 +0530

    added synchronisation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const { PORT } = require('./config/serverConfig');
 const bodyParser = require('body-parser');
 
 const ApiRoutes = require('./routes/index');
+// const{Airport ,City} = require("./models/index");
+const db = require("./models/index");
 
 const setupAndStartServer =async ()=>{
     // create an express object
@@ -12,26 +14,22 @@ const setupAndStartServer =async ()=>{
 
 app.use('/api', ApiRoutes);
 
-    app.listen(PORT , ()=>{
+    app.listen(PORT , async ()=>{
         console.log(`Server started at ${PORT}`);
+
+    // db / model synchronization
+    if(process.env.SYNC_DB){
+
+        db.sequelize.sync({alter :true})
+    }
+    // const city = await City.findOne({
+    //     where:{
+    //         id: 5
+    //     }
+    // });
+    // const airports= await city.getAirports();
+    // console.log(city,airports);
     });
 }
 
 setupAndStartServer();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
